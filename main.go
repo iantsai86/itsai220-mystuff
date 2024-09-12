2d93f223c215288bd724ca413ab771b9fd0ab7e0
Ian Tsai <iantsai86@gmail.com>
2024-09-12 10:40:05 -0700

    fix fibanocci func

--- main.go
+++ main.go
@@ -13,23 +13,6 @@ var (
 	mu            sync.Mutex
 )
 
-func main() {
-	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/ready", readyHandler)
-	http.HandleFunc("/payload", payloadHandler)
-	http.HandleFunc("/metrics", metricsHandler)
-
-	log.Println("Service is starting...")
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: nil,
-	}
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed: %v", err)
-	}
-}
-
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	recordRequest("/health")
 	w.WriteHeader(http.StatusOK)
@@ -41,7 +24,7 @@ func readyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func payloadHandler(w http.ResponseWriter, r *http.Request) {
-	recordRequest("/payload")
+	// recordRequest("/payload")
 	n := rand.Intn(20)
 	fibonacci := fibonacciSequence(n)
 	response := map[string]interface{}{
@@ -70,14 +53,30 @@ func fibonacciSequence(n int) []int {
 
 	fib := make([]int, n)
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i <= n-1; i++ {
 		if i == 0 || i == 1 {
 			fib[i] = i
 		} else {
 			fib[i] = fib[i-1] + fib[i-2]
 		}
-
 	}
 
 	return fib
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/ready", readyHandler)
+	http.HandleFunc("/payload", payloadHandler)
+	http.HandleFunc("/metrics", metricsHandler)
+
+	log.Println("Service is starting...")
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
